1a48b30da66a796ca41ea771cd969007bd6b17c3
Jenny Ha <jhuancaan@unsa.edu.pe>
2021-05-20 16:20:36 -0500

    Update command start

--- bot.py
+++ bot.py
@@ -1,16 +1,53 @@
-from telegram.ext import Updater, CommandHandler
+import logging      # Ayuda a ver lo que sucede con el bot y mostrarlo en consola
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+# Variables
+TOKEN = '1862455246:AAHDE6lLYMHHYk7-p_rBSgf_L3CYRkO4IYA'
 
-# Devuelve un mensaje cuando se presional el comando start
+# Configuracion de logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s,"
+)
+logger = logging.getLogger()
+
+# Funciones para comandos
 def start(update, context):
-    update.message.reply_text(
-        'Bienvenido(a) al Chat de consultas de la Escuela Profesional de Ciencia de la Computación (UNSA)')
+    bot = context.bot
+    # chat_Id = update.message.chat_id
+    user_Name = update.effective_user["first_name"]
+    logger.info(f'El usuario {user_Name} ha iniciado(/start) el bot')
+    update.message.reply_text(f'Hola {user_Name}, gracias por usar nuestro bot.\n'
+                              f'A continuación te mostramos los comandos que puedes usar:')
 
+def getBotInfo(update, context):
+    bot = context.bot
+    chat_Id= update.message.chat_id
+    user_Name = update.effective_user["first_name"]
+    logger.info(f'El usuario {user_Name} ha solicitado información sobre el bot')
+    bot.sendMessage(
+        chat_id=chat_Id,
+        parse_mode="HTML",
+        text=f'Hola soy el bot de la <b>Escuela Profesional de Ciencia de la Computación - UNSA</b>'
+    )
 
+# Main Function
 if __name__ == '__main__':
-    updater = Updater(token='1862455246:AAHDE6lLYMHHYk7-p_rBSgf_L3CYRkO4IYA', use_context=True)
-    dp = updater.dispatcher
+    mybot = telegram.Bot(token=TOKEN)
+    print(mybot.getMe()) # Muestra en consola información sobre el bot
 
-    # Ciclo para consultar acciones en el chat
-    updater.start_polling()
-    updater.idle()
+# Updater: se conecta y recibe los mensajes
+updater = Updater(mybot.token, use_context=True)
+
+# Crear el 'despachador'
+dp = updater.dispatcher
+
+# Crear comando y el método (acción del comando)
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("botInfo", getBotInfo))
+
+# Preguntar por mensajes entrantes
+updater.start_polling()
+
+# Terminar bot con ctrl + c
+updater.idle()
